fe967cb1da04bef32fad0ef971df40607aec5aef
709macdonald <709macdonald@gmail.com>
2024-08-22 17:09:53 -0230

    Selecting Folder and reading files

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+// Sidebar.js
+import React, { useState } from "react";
+import FileManager from "./FileManager";
 
 export default function Sidebar() {
-  return <div className="sidebarContents"></div>;
+  const [files, setFiles] = useState([]);
+
+  return (
+    <div className="sidebarBG">
+      <div className="searchParameters">
+        <input
+          type="text"
+          id="searchBar"
+          placeholder="Search for Keywords"
+        ></input>
+        <div className="checkBoxDiv">
+          <div className="checkbox">
+            <input type="checkbox" checked id="searchPDF" readOnly></input>
+            <label htmlFor="searchPDF">Search PDF</label>
+          </div>
+          <div className="checkbox">
+            <input type="checkbox" checked id="searchWord" readOnly></input>
+            <label htmlFor="searchWord">Search Word Docs</label>
+          </div>
+          <div className="checkbox">
+            <input type="checkbox" checked id="searchImages" readOnly></input>
+            <label htmlFor="searchImages">Search Images</label>
+          </div>
+          <div className="checkbox">
+            <input type="checkbox" checked id="searchPages" readOnly></input>
+            <label htmlFor="searchPages">Search Pages Docs</label>
+          </div>
+        </div>
+      </div>
+      <FileManager setFiles={setFiles} />
+      <div className="fileList">
+        {files.map((file, index) => (
+          <div key={index}>{file}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
