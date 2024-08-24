d47bb0a91906400dd9a0c1408d64988a9c247eff
709macdonald <709macdonald@gmail.com>
2024-08-24 13:57:47 -0230

    Reading text in pdf's

--- src/components/FileLister.jsx
+++ src/components/FileLister.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import pdfToText from "react-pdftotext";
 
 const FileLister = ({ handle, setFiles }) => {
   const hasLogged = useRef(false);
@@ -10,9 +11,13 @@ const FileLister = ({ handle, setFiles }) => {
         if (entry.kind === "file") {
           const file = await entry.getFile();
           const url = URL.createObjectURL(file);
+
+          const pdfText = await extractTextFromPDF(file);
+
           fileArray.push({
             name: file.name,
             url: url,
+            text: pdfText,
           });
         }
       }
@@ -28,6 +33,17 @@ const FileLister = ({ handle, setFiles }) => {
       listFiles();
     }
   }, [handle, setFiles]);
+
+  const extractTextFromPDF = (file) => {
+    return new Promise((resolve, reject) => {
+      pdfToText(file)
+        .then((text) => resolve(text))
+        .catch((error) => {
+          console.error("Failed to extract text from PDF:", error);
+          reject("");
+        });
+    });
+  };
 
   return null;
 };
