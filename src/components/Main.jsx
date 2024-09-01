00d0b04a09483dc9f2857573a558958aa24790e7
709macdonald <709macdonald@gmail.com>
2024-09-01 15:56:01 -0230

    moved FileList to the Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import FileList from "./FileList";
 
 export default function Main({ files }) {
   return (
@@ -10,7 +9,21 @@ export default function Main({ files }) {
         </h2>
       </div>
       <div className="mainScreen">
-        <FileList files={files} />
+        <div className="fileList">
+          {files.length > 0 ? (
+            files.map((file, index) => (
+              <div key={index} className="fileDisplay">
+                <i className="fa-regular fa-file-pdf pdfIcon"></i>
+                <p className="pdfText">{file.name}</p>
+                <a href={file.url} target="_blank" rel="noopener noreferrer">
+                  View File
+                </a>
+              </div>
+            ))
+          ) : (
+            <p>No files to display</p>
+          )}
+        </div>
       </div>
     </div>
   );
