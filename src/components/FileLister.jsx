1b6fde4054b4e53267b65d5f8d52bbe7cd321542
709macdonald <709macdonald@gmail.com>
2024-09-01 16:05:41 -0230

    printing files one by one

--- src/components/FileLister.jsx
+++ src/components/FileLister.jsx
@@ -1,39 +1,26 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import pdfToText from "react-pdftotext";
 
 const FileLister = ({ files, setFiles }) => {
-  const hasLogged = useRef(false);
-
   useEffect(() => {
     const listFiles = async () => {
-      const fileArray = [];
-
       for (const file of files) {
         const url = URL.createObjectURL(file);
 
         try {
           const pdfText = await extractTextFromPDF(file);
-          fileArray.push({
-            name: file.name,
-            url: url,
-            text: pdfText || "",
-          });
+          setFiles((prevFiles) => [
+            ...prevFiles,
+            { name: file.name, url, text: pdfText || "" },
+          ]);
         } catch (error) {
           alert(`Failed to extract text from PDF file: ${file.name}`);
           console.error(`Failed to extract text from ${file.name}:`, error);
-          fileArray.push({
-            name: file.name,
-            url: url,
-            text: "",
-          });
+          setFiles((prevFiles) => [
+            ...prevFiles,
+            { name: file.name, url, text: "" },
+          ]);
         }
-      }
-
-      setFiles(fileArray);
-
-      if (!hasLogged.current) {
-        console.log("FileArray:", fileArray);
-        hasLogged.current = true;
       }
     };
 
